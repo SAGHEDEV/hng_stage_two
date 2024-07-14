3a3e2c406f4eeb1b7f2f1f6959c57b3c80530f08
Abdul Hakeem <abdulhakeem080255@gmail.com>
2024-07-15 00:10:28 +0100

    Timbu API consumption correction completition 2

--- src/pages/main/checkout/coonfirmModal.jsx
+++ src/pages/main/checkout/coonfirmModal.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { CiCircleCheck } from "react-icons/ci";
 import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { CartState } from "../../../utilities/management/state";
 
 const CoonfirmModal = ({ statusShow }) => {
+  const [_, setCarts] = useRecoilState(CartState);
   return (
     <div
       className={`w-full lg:w-[100vh] h-[100%] fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center p-5 md:p-0 ${
@@ -12,7 +15,7 @@ const CoonfirmModal = ({ statusShow }) => {
       <div className="w-[100%] h-[100%] fixed top-0 left-0 right-0 bottom-0 bg-black/30"></div>
       <div className="bg-white p-8 lg:p-10 rounded-md z-20 self-center relative lg:left-[50%] lg:right-[50%] flex justify-center items-center flex-col">
         <p className="text-[16px] lg:text-[32px] p-5 text-center">
-          Are you sure you want to complete the order?
+          Order Completed!!!
         </p>
         <p className="text-[12px] lg:text-[16px] font-normal text-center ">
           The product you ordered will be delivered in your address within
@@ -25,7 +28,10 @@ const CoonfirmModal = ({ statusShow }) => {
 
         <div className="flex justify-center items-center mt-4">
           <Link to="/">
-            <button className="border bg-[#024E82] text-white rounded-md px-12 py-3  active:scale-95 font-bold text-[14px] lg:text-[16px]">
+            <button
+              className="border bg-[#024E82] text-white rounded-md px-12 py-3  active:scale-95 font-bold text-[14px] lg:text-[16px]"
+              onClick={() => setCarts([])}
+            >
               Return to store
             </button>
           </Link>
